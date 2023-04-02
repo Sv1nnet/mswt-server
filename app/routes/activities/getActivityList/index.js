aa27d5bfb45c610aac86aaa4697ba737e1650f60
Leonid Tsukanov <sv1nnet1992@gmail.com>
2023-04-02 17:48:00 +0300

    activity seearch added;

--- app/routes/activities/getActivityList/index.js
+++ app/routes/activities/getActivityList/index.js
@@ -1,9 +1,6 @@
 import Activity from 'models/Activity';
 import Workout from 'models/Workout';
 import Exercise from 'models/Exercise';
-import User from 'models/User';
-import { Types } from 'mongoose';
-import { createRequestError, createResponseError } from 'utils/createResponseError';
 import { createResponse } from '@/app/utils/createResponse';
 import { pickActivityList } from '@/app/utils/pickObjectFromMDBDoc';
 import getUserOrThrow from '@/app/utils/getUserOrThrow';
@@ -12,24 +9,60 @@ import getUserOrThrow from '@/app/utils/getUserOrThrow';
 const getActivityList = async (req, res) => {
   const { id } = req.user;
   console.log("req.params", req.query)
-  let { page = 1, byPage = 30 } = req.query
+  let { page = 1, byPage = 30, searchValue = '' } = req.query
 
   try {
     let user = await getUserOrThrow(id)
 
     page = +page
     byPage = +byPage
+    searchValue = searchValue.toLowerCase()
+
     const startIndex = ((page - 1) * byPage)
     const endIndex = startIndex + (byPage)
 
-    let activities = await Activity.find({ '_id': { $in: user.activities.slice(startIndex, endIndex) } }).limit(31)
-    const total = user.activities.length
-    const workoutsInActivities = await Workout.find({ '_id': { $in: activities.map(activity => Types.ObjectId(activity.workout_id)) }})
-    activities = activities.sort((a, b) => b._doc.index - a._doc.index).map(activity => ({ ...activity._doc, workout_title: workoutsInActivities.find(workout => workout._id.toString() === activity.workout_id).title }))
+    /*
+      - найти все активности пользователя
+      - найти все воркауты пользователя
+      - отфильтровать воркауты по сёрчВэлю
+      - найти все активности содержащие воркаут из массива воркаутов
+    */
+
+    let activities = (await Activity.find({
+      '_id': {
+        $in: user.activities
+      }
+    })).filter(activity => 'workout_id' in activity).sort((a, b) => b._doc.index - a._doc.index)
+    // .limit(byPage + 1)
+
+    let workouts = (await Workout.find({
+      'title': {
+        $regex: new RegExp(searchValue), $options: "i" ,
+      },
+      '_id': {
+        $in: user.workouts,
+      },
+      'is_in_activity': true
+    }))
+
+    let workoutIds = workouts.map(workout => workout._id.toString())
+    activities = activities.filter(activity => workoutIds.includes(activity.workout_id.toString()))
+
+
+    let total = activities.length
+
+    activities = activities.slice(startIndex, endIndex)
+
+    activities = activities
+      .sort((a, b) => b._doc.index - a._doc.index)
+      .map(activity => ({
+        ...activity._doc,
+        workout_title: workouts.find(workout => workout._id.toString() === activity.workout_id.toString()).title
+      }))
 
     const exercisesInActivity = (await Exercise.find({
       '_id': {
-        $in: workoutsInActivities.reduce((acc, workout) => acc.concat(workout.exercises.map(({ id }) => id)), [])
+        $in: workouts.reduce((acc, workout) => acc.concat(workout.exercises.map(({ id }) => id)), [])
       }
     })).map(exercise => exercise._doc)
 
