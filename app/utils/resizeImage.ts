e011cbefd4f459869802609d563406f23da46720
Leonid Tsukanov <sv1nnet1992@gmail.com>
2023-12-10 15:24:47 +0300

    uploaded images directory fixed; image rotation bug fixed; text change commit messsage;

--- app/utils/resizeImage.ts
+++ app/utils/resizeImage.ts
@@ -4,14 +4,30 @@ import fs from 'fs'
 export const MAX_IMAGE_SIDE_SIZE = 320
 
 export const resizeImage = async (rawImage, userDir, targetDir, fileName, files) => {
-  const jimpImage = await Jimp.read(rawImage)
+  let jimpImage = await Jimp.read(rawImage)
   const { width, height } = jimpImage.bitmap;
 
   if (width > MAX_IMAGE_SIDE_SIZE || height > MAX_IMAGE_SIDE_SIZE) {
-    files.fileToSave = await jimpImage.resize(
+    const [isVertical, isHorizontal] = [width < height, width > height]
+
+    let resizedImage = await jimpImage.resize(
       width >= height ? MAX_IMAGE_SIDE_SIZE : Jimp.AUTO,
       height >= width ? MAX_IMAGE_SIDE_SIZE : Jimp.AUTO,
     ).getBufferAsync(`image/${jimpImage.hasAlpha() ? 'png' : 'jpeg'}`)
+
+    jimpImage = await Jimp.read(resizedImage)
+
+    const { width: resizedWidth, height: resizedHeight } = jimpImage.bitmap;
+    const [isResizedVertical, isResizedHorizontal] = [resizedWidth < resizedHeight, resizedWidth > resizedHeight]
+
+    if (isResizedVertical !== isVertical || isResizedHorizontal !== isHorizontal) {
+      resizedImage = await jimpImage
+        .rotate(89)
+        .rotate(1)
+        .getBufferAsync(`image/${jimpImage.hasAlpha() ? 'png' : 'jpeg'}`)
+    }
+
+    files.fileToSave = resizedImage;
 
     if (!fs.existsSync(userDir)){
       fs.mkdirSync(userDir);
